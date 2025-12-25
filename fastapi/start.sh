331ba8e820ff748ad3e21ecd986d293252b97e4b
anuragkrishna <anuragkrishnavennela@gmail.com>
2025-12-25 11:37:19 +0530

    Revert PORT changes and deploy retrained model

--- fastapi/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Railway startup script that reads PORT environment variable
-
-# Get PORT from environment, default to 8001 if not set
-PORT=${PORT:-8001}
-
-echo "Starting uvicorn on port $PORT"
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
